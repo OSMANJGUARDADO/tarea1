8565e853c30abb90247dcd6bb402a3f6c14dbf6f
OSMANJGUARDADO <osmanjavier200301@gmail.com>
2024-06-21 22:39:46 -0600

    first commit

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import path, { resolve } from "node:path";
 export default defineConfig(
     {
         appType: "mpa",
-        base: "/pw2_vitempa/",
+        base: "/tarea1/",
         build: {
             rollupOptions: {
                 input: Object.fromEntries(
